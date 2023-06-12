36c49e2a0a1b1075fe374fda586a9352e971ded9
Vinzenz Leutenegger <vleutenegger@gmail.com>
2023-06-12 19:19:57 +0200

    feature(Lang): add french string translations

---
+++ translations/fr/site.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "first_name" => "Prénom",
+    "last_name" => "Nom de famille",
+    "email" => "Émail",
+    "location" => "Lie",
+    "optional" => "optionnel",
+    "subscribe" => "abonner",
+    "newsletter" => "Newsletter",
+];
