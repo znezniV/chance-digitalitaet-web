8d7a71537d4ecc4ff0153dbf93fd13d01b9e500d
Vinzenz Leutenegger <vleutenegger@gmail.com>
2023-06-12 14:48:30 +0200

    feature(Newsletter): add newsletter component

--- translations/de/site.php
+++ translations/de/site.php
@@ -1,4 +1,11 @@
 <?php
 
 return [
+    "first_name" => "Vorname",
+    "last_name" => "Nachname",
+    "email" => "E-Mail",
+    "location" => "Ort",
+    "optional" => "optional",
+    "subscribe" => "Abonnieren",
+    "newsletter" => "Newsletter",
 ];
