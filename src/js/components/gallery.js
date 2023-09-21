e5f61d0c81f4aadb44a2c88be6d9f5993942927c
Vinzenz Leutenegger <vleutenegger@gmail.com>
2023-09-21 15:37:14 +0200

    feature(Gallery): add gallery component to blog content blocks

---
+++ src/js/components/gallery.js
@@ -0,0 +1,17 @@
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
+import PhotoSwipe from 'photoswipe';
+
+export default () => ({
+    init() {
+        const lightbox = new PhotoSwipeLightbox({
+            gallery:  this.$refs.container,
+            children: '.gallery__image',
+            pswpModule: PhotoSwipe,
+            secondaryZoomLevel: 1,
+            maxZoomLevel: 1,
+            padding: { top: 60, bottom: 60, left: 60, right: 60 }
+        });
+        lightbox.init();
+    }
+});
